f0e8094757e098be1bbc8386b7556a8d7e5df017
Arthur <jovart.arthur@gmail.com>
2013-01-08 21:04:03 +0100

    Add python.py

---
+++ python.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+
+# Couleurs
+VERT="\033[1;32m"
+NORMAL="\033[0;39m"
+ROUGE="\033[1;31m"
+ROSE="\033[1;35m"
+BLEU="\033[1;34m"
+BLANC="\033[0;02m"
+BLANCLAIR="\033[1;08m"
+JAUNE="\033[1;33m"
+CYAN="\033[1;36m"
+
+print("repo python")
